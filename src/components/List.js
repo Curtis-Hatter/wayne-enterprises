92ed76597b03bea9368e01402b6c13a5b85fc4fc
Curtis H <sitrucrettah1@gmail.com>
2021-03-29 19:55:04 -0700

    Add filter

--- src/components/List.js
+++ src/components/List.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./fonts/batmfa__.ttf";
 import Comparison from "./Comparison";
 
+
 class List extends Component{
   constructor(props) {
      super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
@@ -17,10 +18,32 @@ class List extends Component{
      }
   }
 
+  reset(){
+   let newEmployees = [];
+   newEmployees.push(
+      { id: "008f319e3c636b8e", name: 'Wasif', age: 21, email: 'wasif.asmil@example.com', Want_Benefits: 0},
+      { id: "6bd0cab9d7a9c057", name: 'Ol Mama', age: 49, email: 'simplywants.tofeedchildren@example.com', Want_Benefits: 1},
+      { id: "8e0d36e61ce3fc7a", name: 'Sad', age: 16, email: 'sad.greatpain@example.com', Want_Benefits: 0},
+      { id: "177bb39f3ee245bd", name: 'Wubba', age: 25, email: 'wubba.lubba@example.com', Want_Benefits: 1}
+   )
+     for(let i =0; i<11; i++){
+      fetch("https://randomuser.me/api/")
+       .then(res => res.json())
+       .then(result =>{
+          
+          newEmployees.push({id: result.info.seed, name: result.results[0].name.first, age: result.results[0].dob.age, email:result.results[0].email, Want_Benefits: Math.floor(Math.random()*2)});
+
+          this.setState({
+             employees: newEmployees
+          })
+       });
+   }
+  }
   componentDidMount(){
    alert("WELCOME TO WAYNE ENTERPRISES");
    alert("To Sort your Employees by column, click on the column headers. There are also footers that you can click on to fire the employees that want benefits.");
    alert("And after firing those pesky freeloaders, you can simply add more employees by clicking on the 'ADD EMPLOYEES' button.")
+   alert("Finally, to filter an employee out: type in the name in the input field and the employee containing that name will display. You can reset your employees by clicking on 'EMPLOYEES' in the header.")
      for(let i =0; i<11; i++){
      fetch("https://randomuser.me/api/")
       .then(res => res.json())
@@ -121,17 +144,42 @@ class List extends Component{
   })
 }
 
-renderTableFooter() {
+ renderTableFooter() {
   let header = Object.keys(this.state.employees[0])
   return header.map((key, index) => {
      return <th key={index} onClick={this.FilterInfo.bind(this)}>{key.toUpperCase()}</th>
   })
 }
 
+   handleInputChange = event => {
+      // Getting the value and name of the input which triggered the change
+      const { value } = event.target;
+      // console.log(value);
+      // Updating the input's state
+      const filteredEmployees = this.state.employees.filter((employee) => {
+         // console.log(employee.name);
+         return employee.name.includes(value);
+      })
+      
+      if(filteredEmployees.length !== 0)
+      {
+         // console.log(filteredEmployees);
+         this.setState({employees: filteredEmployees});
+      }
+      
+   };
+
   render() { //Whenever our class runs, render method will be called automatically, it may have already defined in the constructor behind the scene.
      return (
         <div className="container margin" >
-           <h1>Here are your employees Mister Wayne</h1>
+           <h1>Here are your <span onClick={this.reset.bind(this)}>employees </span> Mister Wayne</h1>
+           <input className="form-control " aria-label="Small" aria-describedby="inputGroup-sizing-sm" 
+           value={this.state.name}
+           name="name"
+           onChange={this.handleInputChange}
+           type="text"
+           placeholder="Name"
+           />
            <table id='list'>
                <tbody>
                   <tr>{this.renderTableHeader()}</tr>
